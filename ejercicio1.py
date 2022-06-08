00c4171f8bb571faab5cb8d2d03c9c3e39851267
yaravivillegas <yaravi.villegas@gmail.com>
2022-06-08 10:28:42 -0400

    se crea matriz 3x3 aleatoria

---
+++ ejercicio1.py
@@ -0,0 +1,18 @@
+'''
+Crear un arreglo de dos dimensiones de tamaño 3 x 3, 
+con elementos aleatorios de números enteros del 
+0 al 100.
+'''
+import numpy as np
+import random
+
+#matriz = np.zeros((3,3),dtype=int)
+#matriz = np.diag([1,1,1])
+matriz = np.ones((3,3),dtype=int)
+print(matriz)
+
+for i in range(3):
+    for j in range(3):
+        matriz[i][j] = random.randint(0,100)
+        
+print(matriz)
